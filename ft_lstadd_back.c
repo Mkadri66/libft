a0064ff06b85b68291b3b21c70ad146edf77d0d8
Mustapha Kadri <mustaphakadri@MacBook-Air-de-Mustapha.local>
2023-11-02 11:35:52 +0100

    complete ft_lstadd_back.c + norminette

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: mkadri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 18:56:43 by mkadri            #+#    #+#             */
-/*   Updated: 2023/11/01 18:57:42 by mkadri           ###   ########.fr       */
+/*   Updated: 2023/11/02 11:34:36 by mkadri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	
+	t_list	*temp;
+
+	if (*lst == NULL)
+		*lst = new;
+	else
+	{
+		temp = *lst;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
 }
