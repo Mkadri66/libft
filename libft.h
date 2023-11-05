177e35c3952d3d5be6e4895db49765a60b681f3c
Mustapha Kadri <mustaphakadri@MacBook-Air-de-Mustapha.local>
2023-11-05 21:13:27 +0100

    complete itoa and lstmap  + norminette

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mkadri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 19:15:34 by mkadri            #+#    #+#             */
-/*   Updated: 2023/11/02 17:33:44 by mkadri           ###   ########.fr       */
+/*   Updated: 2023/11/05 20:43:25 by mkadri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,10 +69,12 @@ int			ft_strncmp(const char *first, const char *second, size_t lenght);
 
 t_list		*ft_lstnew(void *content);
 t_list		*ft_lstlast(t_list *lst);
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 void		ft_lstadd_front(t_list **lst, t_list *new);
 int			ft_lstsize(t_list *lst);
 void		ft_lstadd_back(t_list **lst, t_list *new);
 void		ft_lstdelone(t_list *lst, void (*del)(void*));
 void		ft_lstclear(t_list **lst, void (*del)(void*));
 void		ft_lstiter(t_list *lst, void (*f)(void *));
+
 #endif
